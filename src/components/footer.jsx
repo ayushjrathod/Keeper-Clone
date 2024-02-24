fa168431f2f423df7e163209df23d2b6a844a27d
Ayush Rathod <111508368+ayushjrathod@users.noreply.github.com>
2024-02-24 15:52:25 +0530

    Add files via upload

---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <footer>
+           <p>All Rights Reserved 2024</p> 
+        </footer>
+        
+    );
+};
+
+export default Footer;
